0f90925762ad7bba664095b46f6e2ffe26951b08
Takahirokun <takkun1789@gmail.com>
2023-03-14 03:29:54 +0900

    feature: インストール用のshell scriptを追加

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Install Requirements
+
+# apt update and install
+apt update 
+apt install unzip
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install deno 
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
